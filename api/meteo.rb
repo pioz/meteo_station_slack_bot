a7ed3446aa18b1a0bc8a5c2c5d1449a73fdbf523
pioz <epilotto@gmx.com>
2024-05-02 16:02:13 +0200

    Do not crash on empty body

--- api/meteo.rb
+++ api/meteo.rb
@@ -15,18 +15,21 @@ end
 
 
 Handler = Proc.new do |request, response|
-  request_body = JSON.parse(request.body.read)
-  case request_body['type']
-  when 'url_verification'
-    response.status = 200
-    response.body = request_body['challenge']
-  when 'event_callback'
-    data = collection.find.last
-    slack_client.chat_postMessage(channel: request_body['event']['channel'], text: "Data: #{data.to_json}", as_user: true)
-    response.status = 200
-    response.body = 'Event processed'
-  else
-    response.status = 400
-    response.body = 'Bad request'
+  if request.body.present?
+    request_body = JSON.parse(request.body.read)
+    case request_body['type']
+    when 'url_verification'
+      response.status = 200
+      response.body = request_body['challenge']
+      return
+    when 'event_callback'
+      data = collection.find.last
+      slack_client.chat_postMessage(channel: request_body['event']['channel'], text: "Data: #{data.to_json}", as_user: true)
+      response.status = 200
+      response.body = 'Event processed'
+      return
+    end
   end
+  response.status = 400
+  response.body = 'Bad request'
 end
