d1f13469544ddf122419895da27463ae71379be6
pioz <epilotto@gmx.com>
2024-05-03 08:56:24 +0200

    Debug

--- api/meteo.rb
+++ api/meteo.rb
@@ -11,15 +11,23 @@ mongo_client = Mongo::Client.new(ENV['MONGO_URI'], { server_api: { version: '1' 
 collection = mongo_client['measurements']
 logger = Logger.new(STDOUT)
 
+def info_body(temperature, humidity, ppm)
+  s = []
+  s << "Temperature: #{temperature.round(2)}C"
+  s << "Humidity: #{humidity.round(2)}%"
+  s << "PPM: #{ppm.round(2)}ppm"
+  return s.join("\n")
+end
+
 Handler = Proc.new do |request, response|
   user_id = request.query['user_id']
   command = request.query['command']
   case command
   when '/meteo'
     # data = collection.find.sort({ _id: -1 }).first
-    slack_client.chat_postMessage(channel: user_id, text: 'Hello from Vercel', as_user: true)
+    # slack_client.chat_postMessage(channel: user_id, text: 'Hello from Vercel', as_user: true)
     response.status = 200
-    response.body = 'Event processed'
+    response.body = info_body(23.0, 45.0, 400.2)
   else
     response.status = 400
     response.body = 'Bad request'
